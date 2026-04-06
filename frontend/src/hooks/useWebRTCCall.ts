61f44aad7f72228f13c789fc94296ec2afa0e398
samman-khanal <samman.samman345@gmail.com>
2026-04-06 13:46:58 +0545

    web rtc

--- frontend/src/hooks/useWebRTCCall.ts
+++ frontend/src/hooks/useWebRTCCall.ts
@@ -71,7 +71,19 @@ function newCallId(): string {
 }
 
 const RTC_CONFIG: RTCConfiguration = {
-  iceServers: [{ urls: ["stun:stun.l.google.com:19302"] }],
+  iceServers: [
+    { urls: ["stun:stun.l.google.com:19302", "stun:stun1.l.google.com:19302"] },
+    {
+      urls: [
+        "turn:openrelay.metered.ca:80",
+        "turn:openrelay.metered.ca:443",
+        "turn:openrelay.metered.ca:443?transport=tcp",
+        "turn:openrelay.metered.ca:80?transport=udp",
+      ],
+      username: "openrelayproject",
+      credential: "openrelayproject",
+    },
+  ],
 };
 
 export function useWebRTCCall() {
